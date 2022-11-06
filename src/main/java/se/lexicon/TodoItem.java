745083daa5d4be2471459dfa04ece9817494942a
azadeh1984 <azadeh_n1984@yahoo.com>
2022-11-06 22:17:09 +0100

    Assignment

--- src/main/java/se/lexicon/TodoItem.java
+++ src/main/java/se/lexicon/TodoItem.java
@@ -1,6 +1,7 @@
 package se.lexicon;
 
 import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
 
 public class TodoItem {
 
@@ -11,43 +12,75 @@ public class TodoItem {
     private boolean done;
 
     private Person creator;
+    private LocalDate deadLine;
 
+    public TodoItem() {
+        this.deadLine = LocalDate.of(2022, 11, 30);
+    }
 
 
     //public void setId(int id){this.id= id;}
-    public int getId(){return id;}
-
-    public void setTitle(String title){
-        if(title== null)throw new IllegalArgumentException("Title param was null");
-        this.title= title;}
-    public String getTitle(){return title;}
-
-    public void setTaskDescription(String taskDescription){
-        this.taskDescription= taskDescription;
+    public int getId() {
+        return id;
     }
-    public String getTaskDescription(){return taskDescription;}
+
+    public void setTitle(String title) {
+        if (title == null) throw new IllegalArgumentException("Title param was null");
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
 
     public void setDeadline(LocalDate deadline) {
-        if(deadline== null)throw new IllegalArgumentException("Deadline param was null");
-        this.deadline = deadline;}
+        if (deadline == null) throw new IllegalArgumentException("Deadline param was null");
+        this.deadline = deadline;
+    }
 
     public LocalDate getDeadline() {
-        return deadline;}
+        return deadline;
+    }
 
-    public void setDone(boolean done){this.done= done;}
-    public boolean isDone(){return done;}
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
 
     public void setCreator(Person creator) {
-        this.creator = creator;}
+        this.creator = creator;
+    }
 
 
     public Person getCreator() {
-        return creator;}
+        return creator;
+    }
 
-    public String getSummary(){
+    public String getSummary() {
         return creator.personInformation();
+
+
+    }
+
+    public boolean isOverDue() {
+        LocalDate currentTime = LocalDate.now();
+        if (currentTime.isAfter(deadLine)) {
+            return true;
+        } else {
+            return false;
+        }
 
     }
 
 }
-
